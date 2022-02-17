bfb7fb7707fbdd49a86c54eb0bca181e5141125c
unknown <patticus.tv@gmail.com>
2022-02-17 18:31:41 +1100

    TS functionality, added route files, created server

--- server.ts
+++ server.ts
@@ -1,2 +1,27 @@
+// # Import: Imports express package 
 const express = require('express');
+// # Set: Set port for use by application
 const PORT = process.env.PORT || 3001;
+
+// Import: Moduler routes
+const apiRoutes = require('/routes/apiRoutes');
+const htmlRoutes = require('/routes/htmlRoutes');
+
+// initialize: app variable by setting it to 'express'
+const app = express();
+
+// # Middlewear: Express middlewear
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+// # Serve: files from the public directory
+app.use(express.static('public'));
+
+// # Utilize: Utilize routes contained in the routes directory
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+// # Listen: listens on defined PORT for connections
+app.listen(PORT, () =>
+    console.log(`Listening @ https://localhost:${PORT}`)
+);
